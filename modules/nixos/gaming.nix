23699a6a8d9cdf24da6420c2231abf56210ea0bf
4rrw <stshalsonixx@gmail.com>
2026-09-07 17:47:27 +0200

    updat

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -13,6 +13,13 @@
   programs.steam.gamescopeSession.enable = true;
   environment.systemPackages = with pkgs; [
     mangohud
+    protonup-ng
   ];
   programs.gamemode.enable = true;
+  
+  # you have to call protonup imperatively :(
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+  };
 }
